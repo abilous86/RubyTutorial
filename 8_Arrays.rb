87e98024614098b6e7371299757bcff1d87fa703
Andrii Bilous <abilous86@icloud.com>
2018-10-20 05:37:10 +0300

    Basic Literals

---
+++ 8_Arrays.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -w
+
+#Array
+arr = ["Andrii", 1, "14.10.1986", "Ukraine,Lviv"];
+arr.each do |x|
+  puts x
+end
+
+#Hash
+hash = colors = {"red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+hash.each do |key, value|
+  print key, " is ", value, "\n"
+end
+
+#Range
+(10..15).each do |i|
+  puts i
+end
